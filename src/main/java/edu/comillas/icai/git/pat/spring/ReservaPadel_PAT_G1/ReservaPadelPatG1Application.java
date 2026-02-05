2e152c8fd969bb852b87db220fa8de75c3467c06
Juan Torres <202304096@alu.comillas.edu>
2026-02-05 10:08:45 +0100

    inital commit

---
+++ src/main/java/edu/comillas/icai/git/pat/spring/ReservaPadel_PAT_G1/ReservaPadelPatG1Application.java
@@ -0,0 +1,13 @@
+package edu.comillas.icai.git.pat.spring.ReservaPadel_PAT_G1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservaPadelPatG1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservaPadelPatG1Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/comillas/icai/git/pat/spring/ReservaPadel_PAT_G1/ReservaPadelPatG1ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.comillas.icai.git.pat.spring.ReservaPadel_PAT_G1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservaPadelPatG1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
