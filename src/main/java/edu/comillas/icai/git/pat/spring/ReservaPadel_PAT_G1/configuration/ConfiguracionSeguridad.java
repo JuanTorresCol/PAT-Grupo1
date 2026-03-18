7d30b35146570edabc657535ce0edb0c15de9acd
carlo <202302444@alu.comillas.edu>
2026-03-18 14:13:00 +0100

    cambios para el merge

--- src/main/java/edu/comillas/icai/git/pat/spring/ReservaPadel_PAT_G1/configuration/ConfiguracionSeguridad.java
+++ src/main/java/edu/comillas/icai/git/pat/spring/ReservaPadel_PAT_G1/configuration/ConfiguracionSeguridad.java
@@ -2,18 +2,12 @@ package edu.comillas.icai.git.pat.spring.ReservaPadel_PAT_G1.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
